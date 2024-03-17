c4413ae3fce125b3969906c13722d91012bf1b73
kokweikhong <kokweikhong@gmail.com>
2024-03-18 00:08:16 +0800

    updates

--- src/app/coming-soon/page.tsx
+++ src/app/coming-soon/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
           height={100}
           className="mb-8 h-40" />
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center text-gray-700 mb-4">Site is under maintenance</h1>
-        <p className="text-center text-gray-500 text-lg md:text-xl lg:text-2xl mb-8">We're working hard to improve the user experience. Stay tuned!</p>
+        <p className="text-center text-gray-500 text-lg md:text-xl lg:text-2xl mb-8">{`We're working hard to improve the user experience. Stay tuned!`}</p>
       </div>
     </div>
   )
