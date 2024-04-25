bc5c9b22901e5da0e0d41a8bbc25f2982f485203
kokweikhong <kokweikhong@gmail.com>
2024-04-25 18:14:15 +0800

    update wood and timber page

--- src/app/(products)/wood-and-timber/_components/timber-installation.tsx
+++ src/app/(products)/wood-and-timber/_components/timber-installation.tsx
@@ -1,11 +1,8 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 const ASSETS_URL = process.env.NEXT_PUBLIC_WEBSITE_ASSETS_URL;
 
-// fences.jpg
-// trellis.jpg
-// decking.jpg
-// stairs.jpg
 const installationIcons: { name: string; img: string }[] = [
   {
     name: "Fences",
@@ -22,6 +19,34 @@ const installationIcons: { name: string; img: string }[] = [
   {
     name: "Stairs",
     img: `${ASSETS_URL}/websites/wood-and-timber/timber-installation-icons/stairs.jpg`,
+  },
+];
+
+const installationAreas: { name: string; img: string; desc: string }[] = [
+  {
+    name: "planter box",
+    img: `${ASSETS_URL}/websites/wood-and-timber/planter-box.jpeg`,
+    desc: `It is very common for our clients to install timber decking on their planter boxes to extend their balconies.`,
+  },
+  {
+    name: "trellis and benches",
+    img: `${ASSETS_URL}/websites/wood-and-timber/trellis-and-benches.jpeg`,
+    desc: `Wood trellis and benches complement each other and creates a natural landscape in the outdoor area.`,
+  },
+  {
+    name: "balcony area",
+    img: `${ASSETS_URL}/websites/wood-and-timber/balcony-area.jpg`,
+    desc: `Wood Decks and plants complement each other and creates a natural landscape in the balcony area.`,
+  },
+  {
+    name: "pool decking area",
+    img: `${ASSETS_URL}/websites/wood-and-timber/pool-decking-area.jpg`,
+    desc: `Natural wood has anti-slipping properties which provides a good base for pool areas.`,
+  },
+  {
+    name: "roof area",
+    img: `${ASSETS_URL}/websites/wood-and-timber/roof-area.jpeg`,
+    desc: `Commercial properties normally convert their roof area into a resting lounge and wood decking is normally the preferred choice as it helps to create a natural landscape for people to relax.`,
   },
 ];
 
@@ -50,6 +75,42 @@ const TimberInstallation = () => {
                 </div>
                 <p className="text-center text-gray-600 font-light font-montserrat text-lg mt-2">
                   {icon.name}
+                </p>
+              </div>
+            ))}
+          </div>
+          <div>
+            {installationAreas.map((area, index) => (
+              <div
+                key={index}
+                className={cn(
+                  "flex flex-col items-center mt-10 md:grid md:grid-rows-[50px_1fr]",
+                  "md:grid-cols-2 md:gap-4 md:items-start",
+                )}
+              >
+                <div className="w-full h-96 relative md:row-span-full">
+                  <Image
+                    src={area.img}
+                    alt="icon"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+                <h3
+                  className={cn(
+                    "uppercase text-2xl font-semibold font-montserrat text-center mt-2",
+                    "md:text-left",
+                  )}
+                >
+                  {area.name}
+                </h3>
+                <p
+                  className={cn(
+                    "text-center text-gray-600 font-light font-montserrat text-lg mt-2",
+                    "md:text-left",
+                  )}
+                >
+                  {area.desc}
                 </p>
               </div>
             ))}
