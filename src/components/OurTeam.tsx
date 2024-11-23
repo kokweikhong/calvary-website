d5eb58f9ff9ac316bd520064330919a58f2d0006
kokwei <kokweikhong@gmail.com>
2024-11-23 19:35:58 +0800

    update kelvin hills to kevin hill

--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -26,7 +26,7 @@ const teams: Team[] = [
   //   youtube: "https://www.youtube.com/watch?v=86V7rBG1O2g",
   // },
   {
-    name: "Kelvin Hiils",
+    name: "Kevin Hiil",
     position: "MET Consultant",
     image: "websites/teams/teams-kelvin.jpg",
     // youtube: "https://www.youtube.com/watch?v=lQiJsOi7etQ&t=106s",
